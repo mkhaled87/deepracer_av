cd909e508b3394010470b7e45eebb30512adf8a7
M. Khaled (MBP) <eng.mk@msn.com>
2020-03-02 09:46:40 +0100

    mods

--- scripts/camera_emulator.py
+++ scripts/camera_emulator.py
@@ -7,13 +7,25 @@ from sensor_msgs.msg import Image
 def get_media_file():
     return "lanefollow_no_stoplines.mov"
 
+
+def wait_for_subscribers()
+    while imagefeed_pub.get_num_connections() < 1:
+        # wait for a connection to publisher
+        rospy.loginfo("Waiting for connection to publisher...")
+        time.sleep(1)
+
+    rospy.loginfo("Connected to publisher.")
+    return
+
 def feed_generator():
+    rospy.loginfo("Loading the media file.")
     video_file = "media" + os.path.sep + get_media_file()
     video = cv2.VideoCapture(video_file)
     fps = video.get(cv2.CAP_PROP_FPS)
     frame_idx = 0
+    rospy.loginfo("Starting the emulated camera stream and we publish to /video_mjpeg.")
     rate = rospy.Rate(fps)
-    while(not rospy.isShuttingDown()):
+    while(True):
         _, image_rgb = video.read()
         frame_idx = frame_idx + 1 
         if frame_idx == video.get(cv2.CAP_PROP_FRAME_COUNT):
