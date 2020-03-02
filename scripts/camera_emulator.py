04fc8d79db768b2096c396a13569fe5c540df430
M. Khaled (MBP) <eng.mk@msn.com>
2020-03-02 09:39:30 +0100

    mods

--- scripts/camera_emulator.py
+++ scripts/camera_emulator.py
@@ -10,15 +10,15 @@ def get_media_file():
 def feed_generator():
     video_file = "media" + os.path.sep + get_media_file()
     video = cv2.VideoCapture(video_file)
-    fps = video.get(cv2.cv.CV_CAP_PROP_FPS)
+    fps = video.get(cv2.CAP_PROP_FPS)
     frame_idx = 0
     rate = rospy.Rate(fps)
     while(not rospy.isShuttingDown()):
         _, image_rgb = video.read()
         frame_idx = frame_idx + 1 
-        if frame_idx == video.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT):
+        if frame_idx == video.get(cv2.CAP_PROP_FRAME_COUNT):
             frame_idx = 0 #Or whatever as long as it is the same as next line
-            video.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, 0)
+            video.set(cv2.CAP_PROP_POS_FRAMES, 0)
 
         image_message = bridge.cv2_to_imgmsg(image_rgb, "bgr8")
         rate.sleep()
