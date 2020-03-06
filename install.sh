b6407a46820eeb4d4d21faf3d2f244eb73338900
Mahmoud Khaled <eng.mk@msn.com>
2020-03-06 16:22:49 +0100

    mods

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+source /opt/aws/deepracer/setup.sh
+source ~/catkin_ws/devel/setup.sh
+
 cd scripts
 sudo chmod +rwx *.py
 
--- set_cal.sh
+++ set_cal.sh
@@ -5,8 +5,8 @@ source ~/catkin_ws/devel/setup.sh
 
 # calibrating
 # rosservice call /set_car_cal calType max mid min polarity
-rosservice call /set_car_cal 0 1830000 1540000 1290000 1
-rosservice call /set_car_cal 1 1603500 1423500 1293000 -1
+rosservice call /set_car_cal -- 0 1830000 1540000 1290000 1
+rosservice call /set_car_cal -- 1 1603500 1423500 1293000 -1
 
 # reset to 0
-rostopic pub -1 /manual_drive ctrl_pkg/ServoCtrlMsg -- 0.9 0.9
+rostopic pub -1 /manual_drive ctrl_pkg/ServoCtrlMsg -- 0.0 0.0
